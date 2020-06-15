ba16446edd656f7cb8129f169706374fe25f0f42
Ken Sodemann <ken@ionic.io>
2020-06-15 16:57:16 -0500

    feat(base): add .prettierrc and remove extra karma

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,40 +1,35 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TabsPage } from "./tabs.page";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: "tabs",
+    path: 'tabs',
     component: TabsPage,
     children: [
       {
-        path: "current-weather",
-        loadChildren: () =>
-          import("../current-weather/current-weather.module").then(
-            m => m.CurrentWeatherPageModule
-          )
+        path: 'current-weather',
+        loadChildren: () => import('../current-weather/current-weather.module').then(m => m.CurrentWeatherPageModule)
       },
       {
-        path: "forecast",
-        loadChildren: () =>
-          import("../forecast/forecast.module").then(m => m.ForecastPageModule)
+        path: 'forecast',
+        loadChildren: () => import('../forecast/forecast.module').then(m => m.ForecastPageModule)
       },
       {
-        path: "uv-index",
-        loadChildren: () =>
-          import("../uv-index/uv-index.module").then(m => m.UvIndexPageModule)
+        path: 'uv-index',
+        loadChildren: () => import('../uv-index/uv-index.module').then(m => m.UvIndexPageModule)
       },
       {
-        path: "",
-        redirectTo: "/tabs/current-weather",
-        pathMatch: "full"
+        path: '',
+        redirectTo: '/tabs/current-weather',
+        pathMatch: 'full'
       }
     ]
   },
   {
-    path: "",
-    redirectTo: "/tabs/current-weather",
-    pathMatch: "full"
+    path: '',
+    redirectTo: '/tabs/current-weather',
+    pathMatch: 'full'
   }
 ];
 
